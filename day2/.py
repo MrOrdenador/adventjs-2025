9ddd656600ed018744e0e39f95f0c9cb9273b027
MrOrdenador <srzaixgd@gmail.com>
2025-12-02 19:07:59 +0100

    Adding another option to the riddle

--- day2/.py
+++ day2/.py
@@ -4,4 +4,11 @@ def manufacture_gifts(gifts_to_produce):
   for item in gifts_to_produce:
     if item["quantity"] > 0:
       result.extend([item["toy"]] * [item["quantity"]])
+      
+    """
+      Another option would be 
+      for item in gifts_to_produce:
+        result.extend([item["toy"]] * max([item["quantity"]], 0))
+    """
+    
   return result
