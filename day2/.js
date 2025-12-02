fe41b735bbdb0f6bbf1891c0ef05301abfd2f86d
MrOrdenador <srzaixgd@gmail.com>
2025-12-02 18:16:59 +0100

    Add day2's .js

---
+++ day2/.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Array<{ toy: string, quantity: number }>} giftsToProduce
+ * @returns {string[]} Array of manufactured gifts
+ */
+function manufactureGifts(giftsToProduce) {
+  let result = []
+  giftsToProduce.forEach(
+    item => {
+      if (item.quantity == 1){
+        result.push(item.toy)
+      } else if (item.quantity > 1){
+        let i = item.quantity;
+        while (i > 0){
+          result.push(item.toy)
+          i = i - 1
+        }
+      }
+    }
+  )
+  return result
+}
