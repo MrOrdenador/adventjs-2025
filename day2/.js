86494fe9705e38c196c63aa0c372e821b90e80ba
MrOrdenador <srzaixgd@gmail.com>
2025-12-02 19:10:24 +0100

    Adding another options to the riddle, by Bryan Herrera

--- day2/.js
+++ day2/.js
@@ -3,19 +3,21 @@
  * @returns {string[]} Array of manufactured gifts
  */
 function manufactureGifts(giftsToProduce) {
-  let result = []
-  giftsToProduce.forEach(
-    item => {
-      if (item.quantity == 1) {
-        result.push(item.toy)
-      } else if (item.quantity > 1){
-        let i = item.quantity;
-        while (i > 0){
-          result.push(item.toy)
-          i--
-        }
+  let result = [];
+  giftsToProduce.forEach((item) => {
+    if (item.quantity == 1) {
+      result.push(item.toy);
+    } else if (item.quantity > 1) {
+      let i = item.quantity;
+      while (i > 0) {
+        result.push(item.toy);
+        i--;
       }
     }
-  )
-  return result
+  });
+  /*
+    A shorter and cleaner option would be to use
+    giftsToProduce.flatMap(gift => Array(Math.max(0, gift.quantity)).fill(gift.toy))
+  */
+  return result;
 }
--- day2/.ts
+++ day2/.ts
@@ -1,19 +1,21 @@
 function manufactureGifts(
-  giftsToProduce: Array<{ toy: string, quantity: number }>
-  ): string[]  {
-  let result : Array<string> = []
-  giftsToProduce.forEach(
-    item => {
-      if (item.quantity == 1){
-        result.push(item.toy)
-      } else if (item.quantity > 1){
-        let i = item.quantity;
-        while (i > 0){
-          result.push(item.toy)
-          i--
-        }
+  giftsToProduce: Array<{ toy: string; quantity: number }>
+): string[] {
+  let result: Array<string> = [];
+  giftsToProduce.forEach((item) => {
+    if (item.quantity == 1) {
+      result.push(item.toy);
+    } else if (item.quantity > 1) {
+      let i = item.quantity;
+      while (i > 0) {
+        result.push(item.toy);
+        i--;
       }
     }
-  )
-  return result
+  });
+  /*
+    A shorter and cleaner option would be to use
+    giftsToProduce.flatMap(gift => Array(Math.max(0, gift.quantity)).fill(gift.toy))
+  */
+  return result;
 }
