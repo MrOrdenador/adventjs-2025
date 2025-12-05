81ce851ca6c782125733eebb3ae183744c436363
MrOrdenador <srzaixgd@gmail.com>
2025-12-05 17:03:34 +0100

    Adding the day5/.ts solution

---
+++ day5/.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} fromTime - The current time in elf format
+ * @param {string} takeOffTime - The take off time in elf format
+ * @returns {number} The time in seconds until take off
+ */
+function timeUntilTakeOff(fromTime, takeOffTime) {
+  function converter(elftime){
+    elftime = elftime.slice(0, -3)
+
+    let [y, m, extra] = elftime.split("*")
+    let [d, extratime] = extra.split("@")
+    let [hh, mm, ss] = extratime.split("|")
+
+    return new Date(
+      Date.UTC(
+        Number(y),
+        Number(m) - 1,
+        Number(d),
+        Number(hh),
+        Number(mm),
+        Number(ss),
+      )
+    )
+  }
+  return (Math.floor((converter(takeOffTime) - converter(fromTime)) / 1000))
+}
