93d0c18f191259161281603600d442fdddb12218
MrOrdenador <srzaixgd@gmail.com>
2025-12-05 17:03:21 +0100

    Adding the day5/.ts solution

---
+++ day5/.ts
@@ -0,0 +1,27 @@
+type ElfDateTime =
+  `${number}*${number}*${number}@${number}|${number}|${number} NP`
+
+function timeUntilTakeOff(
+  fromTime: ElfDateTime,
+  takeOffTime: ElfDateTime
+): number {
+  function converter(elftime:ElfDateTime){
+    elftime = elftime.slice(0, -3)
+
+    let [y, m, extra] = elftime.split("*")
+    let [d, extratime] = extra.split("@")
+    let [hh, mm, ss] = extratime.split("|")
+
+    return new Date(
+      Date.UTC(
+        Number(y),
+        Number(m) - 1,
+        Number(d),
+        Number(hh),
+        Number(mm),
+        Number(ss),
+      )
+    )
+  }
+  return (Math.floor((converter(takeOffTime) - converter(fromTime)) / 1000))
+}
