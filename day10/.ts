86c1e1bddae2ea0a184cdb698ec8033101632715
MrOrdenador <srzaixgd@gmail.com>
2025-12-11 20:23:33 +0100

    Adding the day10 ts sol

--- day10/.ts
+++ day10/.ts
@@ -1,2 +1,18 @@
-// The solution will be published in ~24h from now.
-// Just doing this so people don't steal my answers
+function maxDepth (s: string): number {
+  let intensity = 0
+  let maxIntensity = 0
+  for (const char of s) {
+    if (char == '[') {
+      intensity++
+      if (intensity > maxIntensity) {
+        maxIntensity = intensity
+      }
+    } else if (char == ']') {
+      intensity--
+      if (intensity < 0) return -1
+    }
+  }
+
+  if (intensity != 0) return -1
+  return maxIntensity == 0 ? -1 : maxIntensity
+}
