74017604b0b534aa6485cda28c394b24c67b3e55
MrOrdenador <srzaixgd@gmail.com>
2025-12-11 20:23:39 +0100

    Adding the day10 py sol

--- day10/.py
+++ day10/.py
@@ -1,2 +1,13 @@
-# The solution will be published in ~24h from now.
-# Just doing this so people don't steal my answers
+def max_depth(s: str) -> int:
+  intensity = maxIntensity = 0
+  for char in s:
+    if char == "[":
+      intensity += 1
+      if intensity > maxIntensity: maxIntensity += 1
+    elif char == "]":
+      intensity -= 1
+      if intensity < 0: return -1
+  if intensity != 0: return -1
+  return maxIntensity if maxIntensity > 0 else -1
+
+
