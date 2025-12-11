e56a6ad39d85d5930335cad10b57a63eb4ee0439
MrOrdenador <srzaixgd@gmail.com>
2025-12-11 20:23:22 +0100

    Adding the day10 sol

--- day10/.js
+++ day10/.js
@@ -1,2 +1,22 @@
-// The solution will be published in ~24h from now.
-// Just doing this so people don't steal my answers
+/**
+ * @param {string} s - The string to check
+ * @returns {number} The maximum depth of the magic
+ */
+function maxDepth(s) {
+  let intensity = 0
+  let maxIntensity = 0
+  for (const char of s) {
+    if (char == '[') {
+      intensity++
+      if (intensity > maxIntensity) {
+        maxIntensity = intensity
+      }
+    } else if (char == ']') {
+      intensity--
+      if (intensity < 0) return -1
+    }
+  }
+
+  if (intensity != 0) return -1
+  return maxIntensity == 0 ? -1 : maxIntensity
+}
