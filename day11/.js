272130a6f4d8aa4e2cc6fea8152fc09515f764bf
MrOrdenador <srzaixgd@gmail.com>
2025-12-12 16:18:34 +0100

    Adding day11s js solution

---
+++ day11/.js
@@ -0,0 +1,25 @@
+/** * 
+ * @param {string[]} warehouse - 
+ * The warehouse layout * @returns {number} 
+* The count of unwatched gifts 
+*/
+function findUnsafeGifts(warehouse) {
+  const warehouseBoard = warehouse.filter((line) => line.trim() !== "");
+  const rows = warehouseBoard.length;
+  const cols = warehouseBoard[0].length;
+  let unprotectedGifts = 0;
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (warehouseBoard[row][col] == "*") {
+        const up = row > 0 && warehouseBoard[row - 1][col] === "#";
+        const down = row < rows - 1 && warehouseBoard[row + 1][col] === "#";
+        const left = col > 0 && warehouseBoard[row][col - 1] === "#";
+        const right = col < cols - 1 && warehouseBoard[row][col + 1] === "#";
+        if (!(up || down || left || right)) {
+          unprotectedGifts++;
+        }
+      }
+    }
+  }
+  return unprotectedGifts;
+}
