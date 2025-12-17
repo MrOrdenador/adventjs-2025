4810ad9bf918e0dfa92a42ec98d324688d84b840
MrOrdenador <srzaixgd@gmail.com>
2025-12-17 23:16:07 +0100

    Adding day17's ts sol

---
+++ day17/.ts
@@ -0,0 +1,43 @@
+function hasFourLights(board: string[][]): boolean {
+  if (!board.length) return false
+
+  const rows = board.length
+  const cols = board[0].length
+
+  function hasStreak(getCell: (i: number) => string, length: number): boolean {
+    let count = 0
+    let prev = '.'
+
+    for (let i = 0; i < length; i++) {
+      const cell = getCell(i)
+
+      if (cell === '.') {
+        count = 0
+        prev = '.'
+      } else if (cell === prev) {
+        count++
+      } else {
+        prev = cell
+        count = 1
+      }
+
+      if (count === 4) return true
+    }
+
+    return false
+  }
+
+  for (let r = 0; r < rows; r++) {
+    if (hasStreak((c) => board[r][c], cols)) {
+      return true
+    }
+  }
+
+  for (let c = 0; c < cols; c++) {
+    if (hasStreak((r) => board[r][c], rows)) {
+      return true
+    }
+  }
+
+  return false
+}
