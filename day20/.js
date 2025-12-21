a1a53049665f7d210c855918e8d2c23e0633bd5f
MrOrdenador <srzaixgd@gmail.com>
2025-12-21 23:09:33 +0100

    Adding day20's .js sol

---
+++ day20/.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[][]} warehouse
+ * @param {number[]} drops
+ * @returns {string[][]}
+ */
+function dropGifts(warehouse, drops) {
+  let newWareHouse = warehouse.map(row => [...row])
+  const rows = warehouse.length
+  
+  for (const col of drops){
+    for (let row = rows - 1; row >= 0; row--){
+      if (newWareHouse[row][col] == "."){
+        newWareHouse[row][col] = "#"
+        break
+      }
+    }
+  }
+
+  return newWareHouse
+}
