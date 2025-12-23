06db0179abb95101ae946f4c1522267c151a878c
MrOrdenador <srzaixgd@gmail.com>
2025-12-23 23:20:30 +0100

    Fixing day2.py

--- day02/.py
+++ day02/.py
@@ -2,13 +2,10 @@ def manufacture_gifts(gifts_to_produce):
   result = []
 
   for item in gifts_to_produce:
-    if item["quantity"] > 0:
-      result.extend([item["toy"]] * [item["quantity"]])
-      
-    """
-      Another option would be 
-      for item in gifts_to_produce:
-        result.extend([item["toy"]] * max([item["quantity"]], 0))
-    """
-    
+    quantity = item["quantity"]
+    toy = item["toy"]
+
+    if type(quantity) == int and quantity > 0:
+      result.extend([toy] * quantity)
+
   return result
