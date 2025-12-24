45e2f67998b1acccfe7d58fce951b167df0daa17
MrOrdenador <srzaixgd@gmail.com>
2025-12-24 09:14:33 +0100

    Fixing day07 py

--- day07/.py
+++ day07/.py
@@ -1,5 +1,5 @@
 def draw_tree(height, ornament, frequency):
-  string = ""
+  tree_drawing = ""
   pos = 1
 
   for i in range(height):
@@ -7,6 +7,6 @@ def draw_tree(height, ornament, frequency):
     for j in range(2*i+1):
       line+= ornament if pos % frequency == 0 else "*"
       pos += 1
-    string += " "*(height-i-1) + line + "\n"
-  string += " "*(height-1) + "#"
-  return string
+    tree_drawing += " "*(height-i-1) + line + "\n"
+  tree_drawing += " "*(height-1) + "#"
+  return tree_drawing
