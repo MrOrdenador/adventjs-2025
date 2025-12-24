88fe86a9cc4d7289cbcb91b48e2358363b358690
MrOrdenador <srzaixgd@gmail.com>
2025-12-24 09:02:48 +0100

    Fixing day05.js to match the ts solution

--- day05/.js
+++ day05/.js
@@ -5,9 +5,9 @@
  */
 function timeUntilTakeOff(fromTime, takeOffTime) {
   function converter(elftime){
-    elftime = elftime.slice(0, -3)
+    let time = elftime.slice(0, -3)
 
-    let [y, m, extra] = elftime.split("*")
+    let [y, m, extra] = time.split("*")
     let [d, extratime] = extra.split("@")
     let [hh, mm, ss] = extratime.split("|")
 
