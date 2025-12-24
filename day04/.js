254fd916b1811e0b34e3563f5c10b123ffd893df
MrOrdenador <srzaixgd@gmail.com>
2025-12-24 08:56:15 +0100

    Fixing day04.js to match day04.ts

--- day04/.js
+++ day04/.js
@@ -4,13 +4,13 @@
  * @returns {string|null} The deciphered PIN or null if invalid
  */
 function decodeSantaPin(code) {
-  code = code.slice(1, -1).split("][");
+  let blocks = code.slice(1, -1).split("][");
   
-  if (code.length != 4) return null;
+  if (blocks.length !== 4) return null;
 
   let result = "";
 
-  for (let hole of code) {
+  for (let hole of blocks) {
     if (hole === "<") {
       result += result.at(-1);
       continue;
@@ -18,10 +18,11 @@ function decodeSantaPin(code) {
 
     let total = Number(hole[0]);
     for (let i = 1; i < hole.length; i++) {
-      if (hole[i] === "+") total = (total + 1) % 10;
-      else if (hole[i] === "-") total = (total - 1 + 10) % 10;
+      const char = hole[i];
+      if (char === "+") total = (total + 1) % 10;
+      else if (char === "-") total = (total - 1 + 10) % 10;
     }
-    
+
     result += total;
   }
 
