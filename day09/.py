e9d26814fedddf967ce9d2703bd8acc262ff8b9b
MrOrdenador <srzaixgd@gmail.com>
2025-12-24 09:29:29 +0100

    Fixing day09.py

--- day09/.py
+++ day09/.py
@@ -1,39 +1,38 @@
 def move_reno(board: str, moves: str):
-    board_array = [line for line in board.split("\n") if line.strip() != ""]
+  board_array = [line for line in board.split('\n') if line.strip() != '']
 
-    allowed_moves = {
-        "R": {"row": 0, "col": 1},
-        "L": {"row": 0, "col": -1},
-        "U": {"row": -1, "col": 0},
-        "D": {"row": 1, "col": 0}
-    }
+  allowed_moves = {
+    'R': {'row': 0, 'col': 1},
+    'L': {'row': 0, 'col': -1},
+    'U': {'row': -1, 'col': 0},
+    'D': {'row': 1, 'col': 0}
+  }
 
-    def out_of_bounds(r: int, c: int) -> bool:
-        return not (0 <= r < len(board_array) and 0 <= c < len(board_array[0]))
+  def out_of_bounds(r: int, c: int) -> bool:
+    return not (0 <= r < len(board_array) and 0 <= c < len(board_array[0]))
 
-    def find_reno():
-        for row in range(len(board_array)):
-            for col in range(len(board_array[row])):
-                if board_array[row][col] == "@":
-                    return {"row": row, "col": col}
-        return None
+  start_row = next(
+    i for i, row in enumerate(board_array) 
+    if '@' in row
+  )
 
-    pos_reno = find_reno()
-    if not pos_reno:
-        return "fail"
 
-    for char in moves:
-        move_data = allowed_moves[char]
-        pos_reno["row"] += move_data["row"]
-        pos_reno["col"] += move_data["col"]
+  start_col = board_array[start_row].index('@')
 
-        if out_of_bounds(pos_reno["row"], pos_reno["col"]):
-            return "crash"
+  pos_reno = {'row': start_row, 'col': start_col}
 
-        next_cell = board_array[pos_reno["row"]][pos_reno["col"]]
-        if next_cell == "*":
-            return "success"
-        if next_cell == "#":
-            return "crash"
+  for char in moves:
+    move_data = allowed_moves[char]
+    pos_reno['row'] += move_data['row']
+    pos_reno['col'] += move_data['col']
 
-    return "fail"
+    if out_of_bounds(pos_reno['row'], pos_reno['col']):
+      return 'crash'
+
+    next_cell = board_array[pos_reno['row']][pos_reno['col']]
+    if next_cell == '*':
+      return 'success'
+    if next_cell == '#':
+      return 'crash'
+
+  return 'fail'
