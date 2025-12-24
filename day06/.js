ea0ee55aa22cb1ca084a14eb1f9421cd50045aef
MrOrdenador <srzaixgd@gmail.com>
2025-12-24 09:05:47 +0100

    fixing day05.js

--- day06/.js
+++ day06/.js
@@ -3,21 +3,27 @@
  * @returns {string[]} Colors of matched pairs
  */
 function matchGloves(gloves) {
-  const counts = {}
+  const pending = {};
+  const result = [];
 
-  gloves.forEach((glove) => {
-    if (!counts[glove.color]) {
-      counts[glove.color] = { L: 0, R: 0 }
+  for (const glove of gloves) {
+    const hand = glove.hand;
+    const color = glove.color;
+
+    if (!pending[color]) {
+      pending[color] = { L: 0, R: 0 };
     }
-    counts[glove.color][glove.hand]++
-  })
 
-  const array = []
-
-  for (const color in counts) {
-    const pairs = Math.min(counts[color].L, counts[color].R)
-    for (let i = 0; i < pairs; i++) array.push(color)
+    if (hand === 'L' && pending[color].R > 0) {
+      pending[color].R--;
+      result.push(color);
+    } else if (hand === 'R' && pending[color].L > 0) {
+      pending[color].L--;
+      result.push(color);
+    } else {
+      pending[color][hand]++;
+    }
   }
 
-  return array
+  return result;
 }
