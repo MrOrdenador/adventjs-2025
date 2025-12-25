4f29359b44ff1b7608a35ebd2fe60fb6a52abfec
MrOrdenador <srzaixgd@gmail.com>
2025-12-25 18:01:49 +0100

    Fixing day16.py

--- day16/.py
+++ day16/.py
@@ -1,13 +1,14 @@
-def pack_gifts(gifts: list[int], maxWeight: int) -> int | None:
+def pack_gifts(gifts: list[int], max_weight: int) -> int | None:
+  # ps: Ik the original arg was maxWeight but I j made it more pythronic
   if not len(gifts): return 0
-  sleighCount = 1
-  currentWeight = 0
+  sleigh_count = 1
+  current_weight = 0
 
-  for giftWeight in gifts:
-    if (giftWeight > maxWeight): return None
-    if giftWeight + currentWeight > maxWeight:
-      currentWeight = giftWeight
-      sleighCount += 1
+  for gift_weight in gifts:
+    if (gift_weight > max_weight): return None
+    if gift_weight + current_weight > max_weight:
+      current_weight = gift_weight
+      sleigh_count += 1
     else:
-      currentWeight += giftWeight
-  return sleighCount
+      current_weight += gift_weight
+  return sleigh_count
