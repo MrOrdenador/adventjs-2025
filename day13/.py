770960951f2ce8abe53d5b067fe2d5a63691b9e2
MrOrdenador <srzaixgd@gmail.com>
2025-12-25 17:58:11 +0100

    Fixing day13.py

--- day13/.py
+++ day13/.py
@@ -1,34 +1,29 @@
 def run_factory(factory: list[str]) -> str:
   pos = { "row": 0, "col": 0 };
-  walkedOn = [];
+  walked_on = set()
   
   while True:
-    if (
-      pos["row"] < 0 or
-      pos["row"] >= len(factory) or
-      pos["col"] < 0 or
-      pos["col"] >= len(factory[0])
-    ):
+    if pos['row'] < 0 or pos['row'] >= len(factory) or pos['col'] < 0 or pos['col'] >= len(factory[0]):
       return "broken"
-      
-    for p in walkedOn:
-      if p["row"] == pos["row"] and p["col"] == pos["col"]:
-        return "loop"
-        
-    move = factory[pos["row"]][pos["col"]]
+
+    pos_key = f"{pos['row']},{pos['col']}"
     
+    if pos_key in walked_on:
+      return "loop"
+
+    move = factory[pos['row']][pos['col']]
     if move == ".":
       return "completed"
-    
-    walkedOn.append({ "row": pos["row"], "col": pos["col"] })
 
-    if move == "<": 
-      pos["col"] -= 1
-    elif move == ">": 
-      pos["col"] += 1
-    elif move == "^": 
-      pos["row"] -= 1
-    elif move == "v": 
-      pos["row"] += 1
+    walked_on.add(pos_key)
+
+    if move == "<":
+      pos['col'] -= 1
+    elif move == ">":
+      pos['col'] += 1
+    elif move == "^":
+      pos['row'] -= 1
+    elif move == "v":
+      pos['row'] += 1
     else:
       return "broken"
