b7bf40bbfb1ce30c5e4d38e300f45dc80028b948
MrOrdenador <srzaixgd@gmail.com>
2025-12-25 18:16:53 +0100

    Adding day22's JS sol

---
+++ day22/.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string[][]} maze
+ * @returns {boolean}
+ */
+function canEscape(maze) {
+  const startRow = maze.findIndex(row => row.includes("S"))
+  let startPoint = [
+    startRow,
+    maze[startRow].indexOf("S")
+  ]
+  const visited = Array.from({ length: maze.length }, () => Array(maze[0].length).fill(false));
+
+  const dirs = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1]
+  ]
+
+  function isValid(i, j) {
+    return i >= 0 && i < maze.length && j >= 0 && j < maze[0].length && maze[i][j] != "#"
+  }
+
+  function dfs(i, j){
+    if (maze[i][j] == "E") return true
+    if (visited[i][j]) return false
+
+    visited[i][j] = true
+
+    for (const [x, y] of dirs){
+      const ni = i + x;
+      const nj = j + y;
+      if (isValid(ni, nj)) {
+        if (dfs(ni, nj)) return true;
+      }
+    }
+    return false
+  }
+  const result = dfs(startPoint[0], startPoint[1])
+  return result
+}
